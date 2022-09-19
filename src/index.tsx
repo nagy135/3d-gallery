cc1e38b6ac2feeca8a7375043eb09c703265f0fa
Viktor Nagy <viktor.nagy@01people.com>
2022-09-19 14:41:57 +0200

    refactor: break it down to components

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,27 @@
 import * as THREE from "three";
 import { createRoot } from "react-dom/client";
 import React, { useRef, useState } from "react";
-import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
+import CameraController from "@components/camera-controller";
+import { CSSProperties } from "react";
+import Box from "@components/box";
 
-function Box(props: ThreeElements["mesh"]) {
-  const ref = useRef<THREE.Mesh>(null!);
-  const [hovered, hover] = useState(false);
-  const [clicked, click] = useState(false);
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-    </mesh>
-  );
-}
+
+const style: CSSProperties = {
+  width: '50vw',
+  height: '50vh',
+  margin: '50% 0 0 50%',
+  transform: 'translate(-50%, -50%)'
+};
 
 createRoot(document.getElementById("root") as HTMLElement).render(
-  <Canvas>
-    <ambientLight />
-    <pointLight position={[10, 10, 10]} />
-    <Box position={[-1.2, 0, 0]} />
-    <Box position={[1.2, 0, 0]} />
-  </Canvas>
+  <div style={style}>
+    <Canvas>
+      <CameraController />
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[2, 0, 0]} />
+      <Box position={[-2, 0, 0]} />
+    </Canvas>
+  </div>
 );
